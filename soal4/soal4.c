43e17668c703d948e5c3df039d50e1cf65bc6287
IDN1945 <elang_makan@yahoo.co.id>
2019-03-15 21:37:12 +0700

    Update soal4.c

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -1,1 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+  pid_t pid, sid;
+
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+  int angka = 1;
+  while(1) {
+    struct stat baca;
+
+    time_t raw;
+    time(&raw);
+
+    char file[100], sehat[100];
+    strcpy(file, "/home/elang/Documents/makanan/makan_enak.txt");
+
+    stat(file, &baca);
+
+    int last = (int)difftime(raw, baca.st_atime);
+
+    if (last <= 30)
+    {
+      sprintf(sehat, "/home/elang/Documents/makanan/makan_sehat%d.txt", angka);
+      FILE *fsehat;
+      fsehat = fopen(sehat, "w");
+      fclose(fsehat);
+      angka++;
+    }
+    sleep(5);
+  }
+
+  exit(EXIT_SUCCESS);
+}
