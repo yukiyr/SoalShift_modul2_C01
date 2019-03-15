8cc76fec4019e5816172aa35725148f7792017d8
yukiyr <37041284+yukiyr@users.noreply.github.com>
2019-03-15 06:50:52 +0000

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,1 +1,91 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
+
+int main() {
+  pid_t pid, sid;
+
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while(1) {
+    int pid, pid1, pid2, pid3; 
+  
+    pid = fork(); 
+  
+    if (pid == 0) { 
+  
+        char *argv[4] = {"chmod", "777", "hatiku/elen.ku", NULL};
+        execv("/bin/chmod", argv);
+
+    } 
+  
+    else { 
+        pid1 = fork(); 
+        if (pid1 == 0) { 
+            char *argv[3] = {"touch", "hatiku/elen.ku", NULL};
+            execv("/usr/bin/touch", argv);
+
+        } 
+        else { 
+            pid2 = fork(); 
+            if (pid2 == 0) { 
+                char *argv[4] = {"mkdir", "-p", "hatiku", NULL};
+                execv("/bin/mkdir", argv);
+
+            } 
+  
+            else { 
+                                char  alamat[] = "/home/test/hatiku/elen.ku";
+                                struct stat file;
+                                if (stat(alamat,&file) == 0) {
+                                struct group *grp;
+                                struct passwd *pwd;
+
+                                grp = getgrgid(file.st_uid);
+                                pwd = getpwuid(file.st_gid);
+
+                                if (strcmp(grp->gr_name, "www-data") != 0 && strcmp(pwd->pw_name, "www-data") != 0) {
+                                        char *argv[3] = {"rm", "/home/test/hatiku/elenku", NULL};
+                                        execv("/bin/rm", argv);
+                                }
+                                }
+            } 
+        } 
+    } 
+    sleep(3);
+  }
+  
+  exit(EXIT_SUCCESS);
+}
