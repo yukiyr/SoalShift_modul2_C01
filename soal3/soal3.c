17ae88ae10d5a7fdbdf764d3ded84e301b4697d8
yukiyr <37041284+yukiyr@users.noreply.github.com>
2019-03-15 13:59:42 +0000

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -4,16 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int p[2];
-int r;
-
 int main() {
+  int p[2];
+  int q[2];
+
   pid_t child_id;
   pid_t child_id_2;
   pid_t child_id_3;
+  pid_t child_id_4;
   int status;
+  int r;
+  char tampungan[10000]={0};
 
-  FILE *out_file = fopen("daftar.txt","w");
   child_id = fork();
 
   if (child_id == 0) {
@@ -35,18 +37,34 @@ int main() {
         char *grep[] = {"grep", ".*.txt$", NULL};
 
         pipe(p);
+        pipe(q);
         child_id_3=fork();
-        if (child_id_3 !=0) {
+        if (child_id_3 == 0) {
+                //this is third child
                 dup2(p[1],1);
                 close(p[0]);
-                r=execvp("ls",ls);
-        } else {
-                dup2(p[0],0);
                 close(p[1]);
-                r=execvp("grep",grep);
-                close(p[0]);
+                execvp("ls", ls);
+        } else {
+                child_id_4 = fork();
+                if (child_id_4 == 0) {
+                        //this is fourth child
+                        dup2(p[0],0);
+                        dup2(q[1],1);
+                        close(p[1]);
+                        close(p[0]);
+                        close(q[1]);
+                        close(q[0]);
+                        execvp("grep", grep);
+                } else {
+                        close(p[0]);
+                        close(p[1]);
+                        close(q[1]);
+                        r = read(q[0],tampungan,sizeof(tampungan));
+                        FILE *out_file = fopen("daftar.txt","w+");
+                        fprintf(out_file,"%.*s\n", r, tampungan);
+                }
         }
-        fprintf(out_file,"%d",r);
     }
   }
 }
